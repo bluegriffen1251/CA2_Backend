1aefa6bf436275e952f3aa5d0852ae84a7699a64
Jagathpriyan <bluegriffen1856@gmail.com>
2025-04-15 09:42:03 +0530

    four

--- index.js
+++ index.js
@@ -21,11 +21,21 @@ app.get('/get',(req,res) =>{
 
 app.put('/put/:id',(req,res) => {
     const id = parseInt(params.users.id);
+    const add = users.length+1;
+    if(!add){
+        return res.status(404).json({message:"Email not found"});
+    }
+    return res.status(200).json({message:"Email updated"});
 
 })
 
 app.delete('/delete/:id',(req,res)=>{
     const id = parseInt(params.users.id)
+    const initial = users.length-1;
+    if(!initial) {
+        return res.status(404).json({message:"Email not found"});
+    }
+    return res.status(200).json({message:"User deleted successfully",initial});
 })
 
 app.listen(PORT,()=>{
